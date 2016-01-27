4487a26560b4f0691085932330ef7f07784fa8ef
baukevw <bauke@baukevanwieren.nl>
2016-01-27 14:21:33 +0100

    no gradient and gem update

--- config/deploy/production.rb
+++
@@ -1,5 +0,0 @@
-set :stage, :test
-server '86.89.195.125:22', user: 'deploy', roles: %w{web app db}
-set :deploy_to, "/home/deploy/rails/stoplight/master"
-set :rails_env, 'production'
-set :branch, 'master'
